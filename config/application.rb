2f50139a56dfccea8a45983a7802e2d361b58a04
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-10 13:41:33 -0300

    feat: add ptbr location to date and hour

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module PersonalDevBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
+    config.i18n.default_locale = :'pt-BR'
+    config.time_zone = 'Brasilia'
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
