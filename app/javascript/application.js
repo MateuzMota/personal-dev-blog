ec412f0f23c483e30276cbbd3b5c6d943ff6763c
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-11 21:39:18 -0300

    feat: add trix to better text editor

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,5 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "./_divider"
+import "trix"
+import "@rails/actiontext"
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
+  has_rich_text :content
 end
