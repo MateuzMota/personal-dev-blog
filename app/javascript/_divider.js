f7a04fb86d3d8780fe036048cc7b7731c403270e
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-14 20:26:25 -0300

    fix: remove duplicate li bug

--- app/javascript/_divider.js
+++ app/javascript/_divider.js
@@ -1,5 +1,10 @@
 document.addEventListener('turbo:load', () => {
+  if (document.URL !== `${document.location.origin}/articles`) return
+
   const posts = document.querySelectorAll('#index__list_of_articles > li');
+  
+  if (posts[0].contains(document.querySelector('.index__divider'))) return
+
   posts.forEach((element, index) => {  
     if (index === posts.length - 1) return
   
