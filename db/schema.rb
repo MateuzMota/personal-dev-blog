eebaf26c59059e6d0c8db3d2a2bf09b0fa483d9b
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-14 19:52:01 -0300

    feat(db): add admin option to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_205547) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_14_224849) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_14_205547) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
